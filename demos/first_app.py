8bdbd2b98184c2f11433a6c68b79f6bb4e11dfa3
Kris Sankaran <sankaran.kris@gmail.com>
2022-03-01 19:04:09 -0600

    trying to update location with dash

--- demos/first_app.py
+++ demos/first_app.py
@@ -16,25 +16,17 @@ app.title = "Download lake labels"
 labels_path = "../data/GL_3basins_2015.shp"
 filter_fields = ["Sub_Basin", "GL_ID"]
 labels = gpd.read_file(labels_path)
-print(labels.head())
 
 # some fixed styling elements
-SIDEBAR_STYLE = {
-    "position": "fixed",
-    "top": 0,
-    "left": 0,
-    "bottom": 0,
-    "width": "16rem",
-    "padding": "2rem 1rem",
-    "background-color": "#f8f9fa",
-}
+basemap_url = 'https://tiles.stadiamaps.com/tiles/alidade_smooth_dark/{z}/{x}/{y}{r}.png'
+titiler_url = "http://127.0.0.1:8000/cog/tiles/{z}/{x}/{y}.png?scale=1&format=png&TileMatrixSetId=WebMercatorQuad&url=..%2Fdata%2Fderived_data%2Fcogs%2Flakes.vrt&resampling_method=nearest&return_mask=true"
 
 map = dl.Map(
-children=[
-    dl.TileLayer(
-        url="http://127.0.0.1:8000/cog/tiles/{z}/{x}/{y}.png?scale=1&format=png&TileMatrixSetId=WebMercatorQuad&url=..%2Fdata%2Fderived_data%2Fcogs%2Flakes.vrt&resampling_method=nearest&return_mask=true"
-    ),
-    dl.GeoJSON( data=labels[:10].to_json(), id="labels", hoverStyle=arrow_function(dict(weight=5, color='#666', dashArray='')))],
+    children=[
+        dl.TileLayer(url=basemap_url, maxZoom=20),
+        dl.TileLayer(url=titiler_url),
+        dl.GeoJSON(data=labels[:10].to_json(), id="labels", hoverStyle=arrow_function(dict(weight=5, color='#666', dashArray='')))
+    ],
     style={'width': '100%', 'height': '50vh', 'margin': "auto", "display": "block"},
     id="map",
     center=(30.3680988015709, 81.2868118286133),
@@ -46,21 +38,31 @@ dropdowns = [dcc.Dropdown(labels[s].unique(), id=f"dropdown-{s}") for s in filte
 
 app.layout = html.Div(
     [
+        dbc.Row([
+        ]),
         dbc.Container(
             [
                 dbc.Row("This is the lake labelling app."),
+                dbc.Row([
+                    dbc.Input("latitude", placeholder="Lat", type="number"),
+                    dbc.Input("longitude", placeholder="Long", type="number"),
+                ]),
                 *dropdowns,
+                map
             ],
-            style=SIDEBAR_STYLE,
             fluid=True,
         ),
-        dbc.Container(
-            [map],
-            fluid=True
-        )
-    ],
+    ]
 )
 
+@app.callback(
+    Input(component_id="latitude", component_property='value'),
+    Input(component_id="longitude", component_property='value'),
+    Input(component_id="map", component_property='value')
+)
+def update_center(latitude, longitude):
+    print(map)
+    map.panTo(40.737, -73.923)
 
 if __name__ == "__main__":
     app.run_server()

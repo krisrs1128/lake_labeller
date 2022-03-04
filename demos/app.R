1c6b3531b371a1a0a4a12bdaf252ba7b8abb8976
Kris Sankaran <sankaran.kris@gmail.com>
2022-03-03 19:23:23 -0600

    allow interactive selection of different bands

--- demos/app.R
+++ demos/app.R
@@ -2,13 +2,17 @@ library(leaflet)
 library(tidyverse)
 library(shiny)
 library(sf)
+library(glue)
 
-titiler_url = "http://127.0.0.1:8000/cog/tiles/{z}/{x}/{y}.png?scale=1&format=png&TileMatrixSetId=WebMercatorQuad&url=..%2Fdata%2Fderived_data%2Fcogs%2Flakes.vrt&resampling_method=nearest&return_mask=true"
+titiler_url <- "http://127.0.0.1:8000/cog/tiles/{{z}}/{{x}}/{{y}}.png?scale=1&format=png&TileMatrixSetId=WebMercatorQuad&url=..%2Fdata%2Fderived_data%2Fcogs%2Flakes.vrt&&bidx={input$ch1}%2C{input$ch2}%2C{input$ch3}&resampling_method=nearest&return_mask=true"
 lakes <- read_sf("../data/GL_3basins_2015.shp")
 
 ui <- fluidPage(
   h3("lake labelling app"),
-  selectInput("gl_id", "Glacier Lake ID", selected = lakes$GL_ID[1], choices = lakes$GL_ID),
+  selectInput("gl_id", "Glacier Lake ID", selected = "GL081218E30060N", choices = lakes$GL_ID),
+  selectInput("ch1", "channel 1", selected = "B1", choices = paste0("B", 1:3)),
+  selectInput("ch2", "channel 2", selected = "B2", choices = paste0("B", 1:3)),
+  selectInput("ch3", "channel 3", selected = "B3", choices = paste0("B", 1:3)),
   leafletOutput("map")
 )
 
@@ -19,11 +23,9 @@ server <- function(input, output) {
     
     leaflet() %>%
       addProviderTiles(providers$Stamen.TonerLite) %>%
-      addTiles(url=titiler_url) %>%
+      addTiles(url=glue(titiler_url)) %>%
       setView(lat=lake$Latitude, lng=lake$Longitude, zoom=12)
   })
-  
-  
 }
 
 shinyApp(ui, server)

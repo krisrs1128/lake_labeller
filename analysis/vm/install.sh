f4cddc02b804bd27cf9e166aa588e2f4f6d8ee5f
Kris Sankaran <sankaran.kris@gmail.com>
2022-11-08 14:34:48 -0600

    all the installation requirements

--- analysis/vm/install.sh
+++ analysis/vm/install.sh
@@ -3,6 +3,11 @@ conda create -n glaciers
 
 conda activate glaciers
 conda install -c conda-forge -y gdal rasterio pyproj shapely
+conda install -c conda-forge -y geopandas pystac-client planetary-computer
 conda install -c conda-forge -y numpy scipy
 conda install -c pytorch -y pytorch torchvision
 conda install -c conda-forge -y torchgeo
+conda install -c conda-forge -y jupyterlab
+
+conda install -c conda-forge -y ipykernel
+python -m ipykernel install --name glaciers

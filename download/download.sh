bafff0367da14f64de8eb1d63d4ceae513d2f755
Kris Sankaran <sankaran.kris@gmail.com>
2023-02-11 22:57:21 -0600

    update end-ix

--- download/download.sh
+++ download/download.sh
@@ -1,4 +1,4 @@
 
 #export PC_SDK_SUBSCRIPTION_KEY=
-python3 download_scenes.py --start_ix $((10 * id)) --end_ix $((10 * id + 9))
+python3 download_scenes.py --start_ix $((10 * id)) --end_ix $((10 * id + 10))
 mv *.tiff /staging/ksankaran/glaciers/

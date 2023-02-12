90b73c7cf5c971e0d9df7bbfbe4991b4302de042
Kris Sankaran <sankaran.kris@gmail.com>
2023-02-11 22:00:36 -0600

    more, larger scenes

--- download/download.sh
+++ download/download.sh
@@ -1,4 +1,4 @@
 
 #export PC_SDK_SUBSCRIPTION_KEY=
-python3 download_scenes.py --start_ix $((2 * Process)) --end_ix $((2 * Process + 1))
+python3 download_scenes.py --start_ix $((10 * Process)) --end_ix $((10 * Process + 9))
 mv *.tiff /staging/ksankaran/glaciers/
